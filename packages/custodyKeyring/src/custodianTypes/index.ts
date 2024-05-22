b5bd3143c90e1e55829c91fa9e111d4666f6f475
rocketxujia <rocketxj@gmail.com>
2024-05-22 09:37:55 +0800

    feat(custodyKeyring): add new JsonApiCustodyKeyring

--- packages/custodyKeyring/src/custodianTypes/index.ts
+++ packages/custodyKeyring/src/custodianTypes/index.ts
@@ -1,114 +1,10 @@
 import { ICustodianType } from "@metamask-institutional/types";
-
-import { BitgoCustodyKeyring } from "./bitgo/BitgoCustodyKeyring";
-import { CactusCustodyKeyring } from "./cactus/CactusCustodyKeyring";
 import { CurvCustodyKeyring } from "./curv/CurvCustodyKeyring";
-import { ECA3CustodyKeyring } from "./eca3/ECA3CustodyKeyring";
 import { JsonRpcCustodyKeyring } from "./json-rpc/JsonRpcCustodyKeyring";
-import { JupiterCustodyKeyring } from "./jupiter/JupiterCustodyKeyring";
-import { QredoCustodyKeyring } from "./qredo/QredoCustodyKeyring";
+import { JsonApiCustodyKeyring } from "./json-api/JsonApiCustodyKeyring";
+
 
 export const CUSTODIAN_TYPES: { [key: string]: ICustodianType } = {
-  QREDO: {
-    name: "Qredo",
-    displayName: "Qredo",
-    apiUrl: "https://api-v2.qredo.network/api/v2",
-    imgSrc: "https://dashboard.metamask-institutional.io/custodian-icons/qredo-icon.svg",
-    icon: "https://dashboard.metamask-institutional.io/custodian-icons/qredo-icon.svg",
-    website: "https://www.qredo.com",
-    onboardingUrl: "https://www.qredo.com",
-    envName: "qredo",
-    keyringClass: QredoCustodyKeyring,
-    production: true,
-    hidden: false,
-    origins: [],
-    environmentMapping: [
-      {
-        pattern: /^.*$/u,
-        mmiApiUrl: "https://mmi.codefi.network/v1",
-      },
-      {
-        pattern: /^https:\/\/api.qredo.network/u,
-        mmiApiUrl: "https://api.mmi-prod.codefi.network/v1",
-      },
-    ],
-  },
-  CACTUS: {
-    name: "Cactus",
-    displayName: "Cactus Custody",
-    apiUrl: "https://api.mycactus.com/custody/v1/mmi-api",
-    imgSrc: "https://dashboard.metamask-institutional.io/custodian-icons/cactus-icon.svg",
-    icon: "https://dashboard.metamask-institutional.io/custodian-icons/cactus-icon.svg",
-    website: "https://www.mycactus.com",
-    onboardingUrl: "https://www.mycactus.com",
-    envName: "cactus",
-    keyringClass: CactusCustodyKeyring,
-    production: true,
-    hidden: false,
-    origins: [],
-    environmentMapping: [
-      {
-        pattern: /^.*$/u,
-        mmiApiUrl: "https://mmi.codefi.network/v1",
-      },
-      {
-        pattern: /^https:\/\/api.mycactus.com/u,
-        mmiApiUrl: "https://api.mmi-prod.codefi.network/v1",
-      },
-    ],
-  },
-  BITGO: {
-    name: "Bitgo",
-    displayName: "BitGo",
-    apiUrl: "https://app.bitgo.com/defi/v2",
-    imgSrc: "https://dashboard.metamask-institutional.io/custodian-icons/bitgo-icon.svg",
-    icon: "https://dashboard.metamask-institutional.io/custodian-icons/bitgo-icon.svg",
-    website: "https://www.bitgo.com",
-    onboardingUrl: "https://www.bitgo.com",
-    envName: "bitgo",
-    keyringClass: BitgoCustodyKeyring,
-    production: true,
-    hidden: false,
-    origins: [],
-    environmentMapping: [
-      {
-        pattern: /^.*$/u,
-        mmiApiUrl: "https://mmi.codefi.network/v1",
-      },
-      {
-        pattern: /^https:\/\/app.bitgo-test.com/u,
-        mmiApiUrl: "https://mmi.codefi.network/v1",
-      },
-      {
-        pattern: /^https:\/\/app.bitgo.com/u,
-        mmiApiUrl: "https://api.mmi-prod.codefi.network/v1",
-      },
-    ],
-  },
-  JUPITER: {
-    name: "Jupiter",
-    displayName: "Jupiter Custody",
-    apiUrl: "https://jupiter-custody.codefi.network",
-    imgSrc: "https://saturn-custody-ui.metamask-institutional.io/saturn.svg",
-    icon: "https://saturn-custody-ui.metamask-institutional.io/saturn.svg",
-    website: "",
-    onboardingUrl: "",
-    envName: "",
-    keyringClass: JupiterCustodyKeyring,
-    production: true,
-    hidden: false,
-    origins: [/^https:\/\/jupiter-custody-ui.codefi.network\//],
-    environmentMapping: [
-      {
-        pattern: /^http:\/\/test-pattern/,
-        mmiApiUrl: "http://test-url",
-      },
-      {
-        pattern: /^http:\/\/localhost.*$/,
-        mmiApiUrl: "http://localhost:3000/v1",
-      },
-    ],
-  },
 
   // All new custodians are an instance of this type
   JSONRPC: {
@@ -128,20 +24,20 @@ export const CUSTODIAN_TYPES: { [key: string]: ICustodianType } = {
   },
 
   // All new custodians are an instance of this type
-  ECA3: {
-    name: "ECA3",
-    displayName: "ECA3",
-    apiUrl: "https://neptune-custody.codefi.network/eth",
-    imgSrc: "https://backend.vistan-brillen.de/storage/files/images/marken/changeme/header/changeme-logo-header.jpg",
-    icon: "https://backend.vistan-brillen.de/storage/files/images/marken/changeme/header/changeme-logo-header.jpg",
-    website: "https://neptune-custody-ui.metamask-institutional.io/",
-    onboardingUrl: "https://neptune-custody-ui.metamask-institutional.io/",
-    envName: "neptune-custody",
-    keyringClass: ECA3CustodyKeyring,
+  JSONAPI: {
+    name: "JSONAPI",
+    displayName: "JSON-API",
+    apiUrl: "",
+    imgSrc: "",
+    icon: "",
+    website: "",
+    onboardingUrl: "", // portal
+    envName: "portal-prod",
+    keyringClass: JsonApiCustodyKeyring,
     production: false,
     hidden: true, // Since this is the prototype, we don't want to show it in the UI
-    origins: [],
-    environmentMapping: [], // No environment mapping for JSON-RPC custodians as this is derived from the configuration service
+    origins: [/.*cobo\.com.*/, /.*localhost.*/],
+    environmentMapping: [], // 
   },
 
   // Legacy Custodian
---
+++ packages/custodyKeyring/src/custodianTypes/json-api/JsonApiCustodyKeyring.ts
@@ -0,0 +1,68 @@
+import { JsonPortalCustodianApi, MMISDK, mmiSDKFactory } from "@metamask-institutional/sdk";
+import {
+  AddressType,
+  AuthTypes,
+  ICustodianTransactionLink,
+  IRefreshTokenAuthDetails,
+  ITransactionStatusMap,
+} from "@metamask-institutional/types";
+
+import { CustodyKeyring } from "../../CustodyKeyring";
+import { ICustodyKeyringOptions } from "../../interfaces/ICustodyKeyringOptions";
+import { JsonRpcStatusMap } from "./JsonRpcStatusMap";
+
+export class JsonApiCustodyKeyring extends CustodyKeyring {
+  public static readonly type = "Custody - JSONAPI";
+  public type = "Custody - JSONAPI";
+
+  public authType = AuthTypes.REFRESH_TOKEN;
+
+  public static addressType: AddressType.POLYCHAIN;
+
+  public readonly custodianType = {
+    name: "JSONAPI",
+    displayName: "JSON-API",
+    apiUrl: "",
+    imgSrc: "",
+    icon: "",
+    website: "",
+    onboardingUrl: "",
+    envName: "portal-prod",
+    keyringClass: JsonPortalCustodianApi,
+    production: false,
+    hidden: true, // Since this is the prototype, we don't want to show it in the UI
+    origins: [],
+    environmentMapping: [], // No environment mapping for JSON-RPC custodians as this is derived from the configuration service
+  };
+
+  sdkFactory = (authDetails: IRefreshTokenAuthDetails, envName: string): MMISDK => {
+    const { refreshTokenUrl, apiUrl } = this.getCustodianFromEnvName(envName);
+
+    authDetails.refreshTokenUrl = refreshTokenUrl;
+
+    return mmiSDKFactory(JsonPortalCustodianApi, authDetails, this.authType, apiUrl);
+  };
+
+  txDeepLink = async (address: string, txId: string): Promise<Partial<ICustodianTransactionLink>> => {
+    const { authDetails, envName } = this.getAccountDetails(address);
+
+    const sdk = this.getSDK(authDetails, envName);
+
+    try {
+      const transactionLink = await sdk.getTransactionLink(txId);
+
+      return transactionLink;
+    } catch (e) {
+      console.log(`Unable to get transction link for ${txId}`);
+      return null;
+    }
+  };
+
+  constructor(opts: ICustodyKeyringOptions = {}) {
+    super(opts);
+  }
+
+  getStatusMap(): ITransactionStatusMap {
+    return JsonRpcStatusMap;
+  }
+}
